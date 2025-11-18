1f7777e03fe125aa1ff21670410ec142c774fdef
David Gavin <david@MACBOOKPRO-3DE8.station>
2025-11-18 19:25:10 +0000

    Add solution to problem number 72
    Solution is a dps algo

--- 72_edit_distance.py
+++ 72_edit_distance.py
@@ -10,7 +10,7 @@ Substitute a character
 Example 1:
 Input: word1 = "horse", word2 = "ros"
 Output: 3
-Explanation: 
+Explanation:
 horse -> rorse (replace 'h' with 'r')
 rorse -> rose (remove 'r')
 rose -> ros (remove 'e')
@@ -18,7 +18,7 @@ rose -> ros (remove 'e')
 Example 2:
 Input: word1 = "intention", word2 = "execution"
 Output: 5
-Explanation: 
+Explanation:
 intention -> inention (remove 't')
 inention -> enention (replace 'i' with 'e')
 enention -> exention (replace 'n' with 'x')
@@ -29,6 +29,33 @@ Constraints:
 0 <= word1.length, word2.length <= 500
 word1 and word2 consist of lowercase English letters.
 """
+
+from functools import lru_cache
+
+
 class Solution:
+
     def minDistance(self, word1: str, word2: str) -> int:
-        pass
+        lenWord1, lenWord2 = len(word1), len(word2)
+
+        @lru_cache()
+        def dps(i, j):
+            if i < 0:
+                # would have to delete j letters for them to match
+                return j + 1
+            if j < 0:
+                return i + 1
+            if word1[i] == word2[j]:
+                # letters match, go left one letter each with no operations needed
+                return dps(i - 1, j - 1)
+            else:
+                # letters don't match, +1 for operation needed, three choices
+                # i-1, j-1: substitute letter
+                # i-1, j: delete from word1
+                # i, j-1: delete from word2
+                return 1 + min([dps(i - 1, j), dps(i, j - 1), dps(i - 1, j - 1)])
+
+        return dps(lenWord1 - 1, lenWord2 - 1)
+
+
+print(Solution().minDistance("abc", "adc"))
